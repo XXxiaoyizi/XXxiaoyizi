92059c7454c513947a2c52cf4b0cc71576129167
XXxiaoyizi <160227755+XXxiaoyizi@users.noreply.github.com>
2025-11-19 22:40:54 +0800

    更新 start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,10 @@
-echo  ra2md.exe
-start ra2md.exe -speedcontrol
-start ra2.exe -speedcontrol
+echo ra2md.exe
+echo ra2.exe
+start ra2md.exe -speedcontrol -unlock -ddraw
+start ra2.exe -speedcontrol -unlock -ddraw
+
+
+
+
+
+
